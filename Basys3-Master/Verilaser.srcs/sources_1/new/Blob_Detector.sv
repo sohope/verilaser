0e12a36493650e9cde5b8098deec4d522753e099
Taeyoung Park <pty3321@kakao.com>
2026-03-06 21:10:15 +0900

    RGB2HSV 임시완료

---
+++ Basys3-Master/Verilaser.srcs/sources_1/new/Blob_Detector.sv
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module Blob_Detector (
+    input logic mask_red,
+    input logic mask_green,
+    input logic mask_blue
+
+);
+
+    logic [$clog2(320*240)-1:0] sum_x, sum_y;
+
+
+
+
+
+endmodule
---
+++ Basys3-Master/Verilaser.srcs/sources_1/new/RGB2HSV.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+module RGB2HSV (
+    input  logic [15:0] pixel_data,
+    output logic        mask_red,
+    output logic        mask_green,
+    output logic        mask_blue
+);
+    logic [7:0] R8, G8, B8; 
+    logic [7:0] RGB_max, RGB_min, RGB_delta; 
+    logic sat_pass;
+
+    // Normalization 
+    assign R8 = {pixel_data[15:11], 3'b000};
+    assign G8 = {pixel_data[10:5],  2'b00};
+    assign B8 = {pixel_data[4:0],   3'b000};
+
+    always_comb begin
+
+        //Value = RGB_MAX
+        RGB_max = (R8 > G8) ? ((R8 > B8) ? R8 : B8) : ((G8 > B8) ? G8 : B8);
+        
+        RGB_min = (R8 < G8) ? ((R8 < B8) ? R8 : B8) : ((G8 < B8) ? G8 : B8);
+
+        RGB_delta = RGB_max - RGB_min;
+
+        // Saturation = RGB_Delta/RGB_MAX
+        sat_pass = (RGB_delta > (RGB_max >> 1));  //Saturation > 50 %? pass 
+
+        mask_red   = (RGB_max == R8) && sat_pass && (RGB_max > 40);
+        mask_green = (RGB_max == G8) && sat_pass && (RGB_max > 40);
+        mask_blue  = (RGB_max == B8) && sat_pass && (RGB_max > 40);
+    end
+
+endmodule
