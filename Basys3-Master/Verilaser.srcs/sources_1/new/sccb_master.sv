3b3fb95a7205fb7776a62c3f575e45e2f56205d6
sohope <msm2570@gmail.com>
2026-03-07 14:40:05 +0900

    sccb_master 수정 완료

--- Basys3-Master/Verilaser.srcs/sources_1/new/sccb_master.sv
+++ Basys3-Master/Verilaser.srcs/sources_1/new/sccb_master.sv
@@ -2,10 +2,10 @@
 
 module sccb_master (
     input  wire       clk,
-    input  wire       reset_n,
-    input  wire       i2c_en,
-    input  wire       i2c_start,
-    input  wire       i2c_stop,
+    input  wire       reset,
+    input  wire       sccb_en,
+    input  wire       sccb_start,
+    input  wire       sccb_stop,
     input  wire [7:0] tx_data,
     output wire       tx_done,
     output wire       tx_ready,
@@ -13,89 +13,45 @@ module sccb_master (
     inout  wire       sda
 );
 
-    sccb_master_core U_SCCB_MASTER_CORE (
-        .clk(clk),
-        .reset(reset),
-        .sccb_en(sccb_en),
-        .sccb_start(sccb_start),
-        .sccb_stop(sccb_stop),
-        .tx_data(tx_data),
-        .tx_done(tx_done),
-        .tx_ready(tx_ready),
-        .scl(scl),
-        .sda(sda)
-    );
-
-endmodule
-
-module sccb_master_core (
-    input  logic       clk,
-    input  logic       reset,
-    input  logic       sccb_en,
-    input  logic       sccb_start,
-    input  logic       sccb_stop,
-    input  logic [7:0] tx_data,
-    output logic       tx_done,
-    output logic       tx_ready,
-    output logic       scl,
-    output wire        sda_o,
-    output wire        sda_oen,     // 1 = drive, 0 = hi-z
-    input  wire        sda_i
-);
-
-    localparam IDLE = 3'h0;
-    localparam START = 3'h1;
-    localparam STOP = 3'h2;
-    localparam WRITE = 3'h3;
-    // localparam READ      = 3'h4;
-    localparam ACK_WRITE = 3'h5;
-    // localparam ACK_READ  = 3'h6;
+    localparam IDLE      = 3'h0;
+    localparam START     = 3'h1;
+    localparam STOP      = 3'h2;
+    localparam WRITE     = 3'h3;
+    localparam ACK_WRITE = 3'h4;
 
     reg [2:0] state, state_next;
     reg [9:0] clk_cnt_reg, clk_cnt_next;
     reg [7:0] tx_data_reg, tx_data_next;
-    // reg [7:0] rx_data_reg, rx_data_next;
     reg [2:0] bit_cnt_reg, bit_cnt_next;
     reg scl_reg, scl_next;
     reg sda_reg, sda_next;
     reg sda_oen_reg, sda_oen_next;
     reg tx_done_reg, tx_done_next;
-    // reg rx_done_reg, rx_done_next;
-    // reg i2c_nack_reg, i2c_nack_next;
 
     assign scl = scl_reg;
-    assign sda_o = sda_reg;
-    assign sda_oen = sda_oen_reg;
+    assign sda = sda_oen_reg ? sda_reg : 1'bz;
     assign tx_done = tx_done_reg;
     assign tx_ready = (state == IDLE) ? 1 : 0;
-    // assign rx_data = rx_data_reg;
-    // assign rx_done = rx_done_reg;
 
-    always @(posedge clk, negedge reset) begin
+    always @(posedge clk, posedge reset) begin
         if (reset) begin
             state        <= IDLE;
             clk_cnt_reg  <= 0;
             tx_data_reg  <= 0;
-            // rx_data_reg  <= 0;
             bit_cnt_reg  <= 0;
             scl_reg      <= 1;
             sda_reg      <= 1;
             sda_oen_reg  <= 0;
             tx_done_reg  <= 0;
-            // rx_done_reg  <= 0;
-            // i2c_nack_reg <= 0;
         end else begin
             state        <= state_next;
             clk_cnt_reg  <= clk_cnt_next;
             tx_data_reg  <= tx_data_next;
-            // rx_data_reg  <= rx_data_next;
             bit_cnt_reg  <= bit_cnt_next;
             scl_reg      <= scl_next;
             sda_reg      <= sda_next;
             sda_oen_reg  <= sda_oen_next;
             tx_done_reg  <= tx_done_next;
-            // rx_done_reg  <= rx_done_next;
-            // i2c_nack_reg <= i2c_nack_next;
         end
     end
 
@@ -103,21 +59,16 @@ module sccb_master_core (
         state_next    = state;
         clk_cnt_next  = clk_cnt_reg;
         tx_data_next  = tx_data_reg;
-        // rx_data_next  = rx_data_reg;
         bit_cnt_next  = bit_cnt_reg;
         sda_next      = sda_reg;
         sda_oen_next  = sda_oen_reg;
         scl_next      = scl_reg;
         tx_done_next  = 0;
-        // rx_done_next  = 0;
-        // i2c_nack_next = i2c_nack_reg;
         case (state)
             IDLE: begin
-                sda_oen_next = 0;  // SDA 해제
+                sda_oen_next = 0;
                 if (sccb_en) begin
-                    case ({
-                        sccb_start, sccb_stop
-                    })
+                    case ({sccb_start, sccb_stop})
                         2'b00: begin
                             state_next   = WRITE;
                             tx_data_next = tx_data;
@@ -128,13 +79,7 @@ module sccb_master_core (
                         2'b10: begin
                             state_next = START;
                         end
-                        // 2'b11: begin
-                        // state_next    = READ;
-                        // sccb_nack_next = sccb_nack;
-                        // end
-                        default: begin
-
-                        end
+                        default: ;
                     endcase
                 end
             end
@@ -196,30 +141,8 @@ module sccb_master_core (
                     end
                 end
             end
-            // READ: begin
-            //     sda_oen_next = 0;  // SDA 해제 (슬레이브가 드라이브)
-            //     scl_next     = 0;
-            //     clk_cnt_next = clk_cnt_reg + 1;
-            //     if (clk_cnt_reg > 249) begin
-            //         scl_next = 1;
-            //         if (clk_cnt_reg == 499) begin
-            //             rx_data_next = {rx_data_reg[6:0], sda};
-            //         end
-            //         if (clk_cnt_reg > 749) begin
-            //             scl_next = 0;
-            //             if (clk_cnt_reg == 999) begin
-            //                 bit_cnt_next = bit_cnt_reg + 1;
-            //                 clk_cnt_next = 0;
-            //                 if (bit_cnt_reg == 7) begin
-            //                     state_next   = ACK_READ;
-            //                     bit_cnt_next = 0;
-            //                 end
-            //             end
-            //         end
-            //     end
-            // end
             ACK_WRITE: begin
-                sda_oen_next = 0;  // SDA 해제 (슬레이브 ACK 수신)
+                sda_oen_next = 0;
                 scl_next     = 0;
                 clk_cnt_next = clk_cnt_reg + 1;
                 if (clk_cnt_reg > 249) begin
@@ -234,23 +157,6 @@ module sccb_master_core (
                     end
                 end
             end
-            // ACK_READ: begin
-            //     sda_next     = i2c_nack_reg;
-            //     sda_oen_next = 1;
-            //     scl_next     = 0;
-            //     clk_cnt_next = clk_cnt_reg + 1;
-            //     if (clk_cnt_reg > 249) begin
-            //         scl_next = 1;
-            //         if (clk_cnt_reg > 749) begin
-            //             scl_next = 0;
-            //             if (clk_cnt_reg == 999) begin
-            //                 clk_cnt_next = 0;
-            //                 state_next   = IDLE;
-            //                 rx_done_next = 1'b1;
-            //             end
-            //         end
-            //     end
-            // end
         endcase
     end
 
