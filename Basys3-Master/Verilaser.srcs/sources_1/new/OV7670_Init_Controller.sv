cb9c791da3a514e0247ccc5614dae12a44e52b55
sohope <msm2570@gmail.com>
2026-03-07 15:08:47 +0900

    SCCB - OV7670_Init_Controller 포트 선언 완료

--- Basys3-Master/Verilaser.srcs/sources_1/new/OV7670_Init_Controller.sv
+++ Basys3-Master/Verilaser.srcs/sources_1/new/OV7670_Init_Controller.sv
@@ -7,9 +7,48 @@ module OV7670_Init_Controller (
     output logic scl
 );
 
+    logic       sccb_en;
+    logic       sccb_start;
+    logic       sccb_stop;
+    logic [7:0] tx_data;
+    logic       tx_done;
+    logic       tx_ready;
+
+    sccb_master U_SCCB_MASTER (
+        .clk       (clk),
+        .reset     (reset),
+        .sccb_en   (sccb_en),
+        .sccb_start(sccb_start),
+        .sccb_stop (sccb_stop),
+        .tx_data   (tx_data),
+        .tx_done   (tx_done),
+        .tx_ready  (tx_ready),
+        .scl       (scl),
+        .sda       (sda)
+    );
+
+    sccb_sequencer U_SCCB_SEQUENCER (
+        .clk       (clk),
+        .reset     (reset),
+        .sccb_en   (sccb_en),
+        .sccb_start(sccb_start),
+        .sccb_stop (sccb_stop),
+        .tx_data   (tx_data),
+        .tx_done   (tx_done),
+        .tx_ready  (tx_ready)
+    );
 endmodule
 
-module sccb_sequencer ();
+module sccb_sequencer (
+    input  logic       clk,
+    input  logic       reset,
+    output logic       sccb_en,
+    output logic       sccb_start,
+    output logic       sccb_stop,
+    output logic [7:0] tx_data,
+    input  logic       tx_done,
+    input  logic       tx_ready
+);
 
 endmodule
 
